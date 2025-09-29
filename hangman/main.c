123ef3c87383548755cfe95a582f778f5a9ecac4
Poramed Thienkriengkrai <poramed.thienkriengkrai@apar.com>
2025-09-29 21:24:33 +0700

    feat: :triangular_flag_on_post: added replay choice

--- hangman/main.c
+++ hangman/main.c
@@ -4,17 +4,18 @@
 #include <time.h>
 #include <stdbool.h>
 
-void setMode(int mode);
-
+// TODO: Replay game (done)
+// store user score (doing)
 void playingHangman(int round, char player[10], char answer[10]);
 
 int main(void)
 {
     srand(time(NULL));
     char player[10] = "";
+    char start[1];
     int round = 0;
+    char score[3];
     int mode = 0;
-    bool start = true;
     char **words = NULL;
     char *volcab[3][50] =
         {{"cat", "dog", "sun", "hat", "book", "fish", "tree", "ball", "star", "cake",
@@ -43,26 +44,50 @@ int main(void)
           "Exile", "Riddle", "Sorcery", "Talisman", "Cipher",
           "Falcon", "Pharaoh", "Labyrinth", "Gargoyle", "Specter"}};
 
+    FILE *fptr;
+    fptr = fopen("score.txt", "w");
+
+    if (fptr != NULL)
+    {
+        printf("success\n");
+        fputs("30", fptr);
+        fclose(fptr);
+
+        fptr = fopen("score.txt", "r");
+        fgets(score, 5, fptr);
+        printf("score : %d\n", atoi(score));
+        fclose(fptr);
+    }
+    else
+    {
+        printf("NOT OPEN\n");
+    }
+
     do
     {
-        printf("Hi! Please select mode \n1)Easy \n2)Medium \n3) Hard\n-1) Exit \n");
-        scanf("%i", &mode);
-        if (mode == -1)
-            return 1;
-    } while (mode > 3 || mode < 1);
+        do
+        {
+            printf("Please select mode \n1)Easy \n2)Medium \n3) Hard\n-1) Exit \n");
+            scanf("%i", &mode);
+            if (mode == -1)
+                return 1;
+        } while (mode > 3 || mode < 1);
 
-    words = volcab[mode - 1];
+        words = volcab[mode - 1];
 
-    int random = rand() % 50;
-    char answer[10];
-    strcpy(answer, words[random]);
+        int random = rand() % 50;
+        char answer[10];
+        strcpy(answer, words[random]);
 
-    printf("%s \n", answer); // for debug
+        printf("%s \n", answer); // for debug
 
-    playingHangman(round, player, answer);
+        playingHangman(round, player, answer);
+        printf("replay (y/n) :");
+        scanf("%s", &start);
+
+    } while (strcmp(start, "y") == 0);
 };
 
-// TODO: handle case sensitive
 void playingHangman(int round, char player[10], char answer[10])
 {
     while (round < 5)
@@ -75,7 +100,7 @@ void playingHangman(int round, char player[10], char answer[10])
 
         if (strcasecmp(player, answer) == 0)
         {
-            printf("You WON the answer is %s", answer);
+            printf("You WON the answer is %s\n", answer);
             break;
         }
 
@@ -93,7 +118,7 @@ void playingHangman(int round, char player[10], char answer[10])
             resultFromPlayer[i] = player[i] == answer[i] ? player[i] : '_';
         }
 
-                printf("result : %s\n", resultFromPlayer);
+        printf("result : %s\n", resultFromPlayer);
 
         if (round == 4)
             printf("You LOOSE the answer is %s\n", answer);
