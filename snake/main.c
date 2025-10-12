4fd0a3f3477e0b11cb2316ff3e00ca941e6c8392
Poramed Thienkriengkrai <poramed.thienkriengkrai@apar.com>
2025-10-12 14:13:33 +0700

    feat(snake): :bug: fix board for prepare input from user

--- snake/main.c
+++ snake/main.c
@@ -5,27 +5,52 @@
 #define ROWS 25
 #define COLS 25
 
-// TODO 
+// TODO
 // create board (done)
 // add snake
 
 void mapBoard(int board[ROWS][COLS]);
 
-void drawBoard(int board[ROWS][COLS], int player[2][2]);
+void drawBoard(int board[ROWS][COLS], int *player);
 
 void updateCell(int map[11][26], int row, int col, int value);
 
-int timer();
+void timer();
 
 int main(void)
 {
     time_t start_time;
     int map[ROWS][COLS];
-    int player[2][2];
+    int player = 33;
     int sizeMap = sizeof(map[0]) / sizeof(map[0][0]);
     int count = 0;
     mapBoard(map);
-    drawBoard(map, player);
+    printf("=============== SNAKE ==================\n\n");
+    while (1)
+    {
+        char input = 119;
+        switch (input)
+        {
+        case 100: // d
+            player++;
+            break;
+        case 119: // w
+            player = player + 25;
+            break;
+        case 97: // a
+            player--;
+            break;
+        case 115: // s
+            player = 25 - player;
+        default:
+            break;
+        }
+
+        drawBoard(map, &player);
+
+        timer();
+    }
+    printf("=======================================\n");
 }
 
 void updateCell(int map[11][26], int row, int col, int value)
@@ -36,63 +61,46 @@ void updateCell(int map[11][26], int row, int col, int value)
 
 void mapBoard(int board[ROWS][COLS])
 {
-    for (int i = ROWS; i >= 0; i--)
+    for (int i = 0; i <= ROWS; i++)
     {
-        for (int j = 1; j <= COLS; j++)
+        for (int j = 1; j <= ROWS; j++)
         {
-            if (i % 2 == 0)
-            {
-                int plusNum = j + (i * 10);
-                board[i][j - 1] = plusNum;
-            }
-            else
-            {
-                int minusNum = (11 - j) + (i * 10);
-                board[i][j - 1] = minusNum;
-            }
+            board[i][j - 1] = j + (i * ROWS); // 1 + 0 * 10 = 2
         }
     }
 }
 
-void drawBoard(int board[ROWS][COLS], int player[2][2])
+void drawBoard(int board[ROWS][COLS], int *player)
 {
-    printf("=============== SNAKE ==================\n\n");
     for (int i = ROWS; i >= 0; i--)
     {
         for (int j = 0; j < COLS; j++)
         {
-            if (i == 0 || i == ROWS || j == COLS - 1 ||  j == 0 )
-            {
+            if (*player < ROWS + 1 || *player > 625)
+                return;
+            else if (*player == board[i][j])
+                printf("O");
+            else if (i == 0 || i == ROWS || j == COLS - 1 || j == 0)
                 printf("#");
-                // printf("%3d ", board[i][j]);
-            }
+            // printf("%3d ", board[i][j]);
             else
-            {
                 printf(" ");
-            }
+
+            // printf("%3d ", board[i][j]);
         }
         printf("\n");
     }
     printf("\n");
-    printf("=======================================\n");
 }
 
-int timer()
+void timer()
 {
     time_t start_time, current_time;
     double elapsed_seconds;
-
-    start_time = time(NULL); // Get current time in seconds since epoch
-
-    // printf("Timer started. Waiting for 5 seconds...\n");
-
+    start_time = time(NULL);
     do
     {
         current_time = time(NULL);
         elapsed_seconds = difftime(current_time, start_time);
-    } while (elapsed_seconds < 1.0); // Loop until 5 seconds have passed
-
-    // printf("1 seconds have passed!\n");
-
-    return 0;
+    } while (elapsed_seconds < 0.1);
 }
